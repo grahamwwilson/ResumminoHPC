38cb7fbd885725898f8529f6ffb67f6eeb0d93dc
grahamwwilson <gwwilson@ku.edu>
2022-08-01 15:35:34 -0500

    Revert to higgsino_slha.in for SLHA template

--- run-N2C1.sh
+++ run-N2C1.sh
@@ -2,7 +2,14 @@
 # 
 # run-N2C1.sh
 #
-# Here we edit the SLHA file on the fly.
+# Here we edit the SLHA file on the fly and the input cards.
+# Input parameters are:
+# 1: ECM
+# 2: MN2
+# 3: MC1
+# 4: MN1
+# 5: PROCESS   (Currently either N2C1Plus or N2C1Minus)
+# 6: PDFSET    (Currently CT14 is supported)
 #
 # Please change E-mail to your own E-mail or turn that feature off!
 #
@@ -17,7 +24,6 @@
 #SBATCH --output=resummino_%A_%a.log  # Standard output and error log
 #SBATCH --error=resummino_%A_%a.err   # Standard output and error log
 
-# Note currently the batch job does NOT yet change the center-of-mass energy from 13 TeV
 ECM=${1:-13000}
 
 pwd
@@ -33,10 +39,13 @@ echo "SLURM_ARRAY_TASK_ID: "${SLURM_ARRAY_TASK_ID}
 # On HPC this should be where you cloned the git repository ResumminoHPC 
 # It is assumed here that you cloned into your $WORK directory (/panfs/pfs.local/work/wilson/$USER
 GITDIR=$WORK/ResumminoHPC
+# The SLHAFile infrastructure is kept with the MyProspinoStuff git repository. 
+# So you need to have cloned MyProspinoStuff into $WORK too.
 SLHADIR=$WORK/MyProspinoStuff/SLHAFiles
-# Directory where the executable resides
+# Directory where the installed Resummino executable resides
 EXEDIR=/panfs/pfs.local/work/wilson/gwwilson/Resummino/ResumminoInstall/bin
 
+# The electroweakino masses (should be specified with trailing .0)
 MN2=${2:-250.0}
 MC1=${3:-235.0}
 MN1=${4:-220.0}
@@ -47,8 +56,18 @@ NMN1=${MN1}E+00
 echo 'New masses = '${NMN2},${NMC1},${NMN1}
 
 PROCESS=${5:-N2C1Plus}
+
+# Note not all LHAPDF sets are likely available.
+# Check in 
+# /panfs/pfs.local/work/wilson/gwwilson/Resummino/resummino-releases/build/lhapdf-prefix/share/LHAPDF 
+
+# Specify PDF set
+PDFSET=${6:-CT14}
+PDFSETLO=${PDFSET}lo
+PDFSETNLO=${PDFSET}nlo
+
 # Directory to run batch job from (Batch Run DIRectory)
-BRDIR=$WORK/ResumminoOut/${PROCESS}-${SLHA}-${ECM}-Job-${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}
+BRDIR=$WORK/ResumminoOut/${PROCESS}-${SLHA}-${ECM}-${PDFSET}-Job-${SLURM_ARRAY_JOB_ID}
 
 # Make sure GSL is found
 echo ${LD_LIBRARY_PATH}
@@ -67,10 +86,10 @@ pwd
 
 SLHAFILE=myslha.in
 
-# First copy over template slha file to batch directory
-cp ${SLHADIR}/higgsino_slha_C1C1_V2.in ${SLHA}.slha
+# First copy over template SLHA file used by LHC SUSY Crosssection working group for N2C1 to batch directory
+cp ${SLHADIR}/higgsino_slha.in ${SLHA}.slha
 
-# Now replace the values appropriately
+# And replace the values appropriately
 sed -i -e "s/{MN2}/${NMN2}/g" -e "s/{MC1}/${NMC1}/g" -e "s/{MN1}/${NMN1}/g" ${SLHA}.slha
 
 if [ ! -e ${SLHAFILE} ]
@@ -78,7 +97,13 @@ then
    ln -s ${SLHA}.slha ${SLHAFILE}
 fi
 
+# Now copy over the Resummino input cards template file
 cp ${GITDIR}/${PROCESS}.in My-${PROCESS}.in
+# And replace the values appropriately. Firstly the center-of-mass energy
+sed -i -e "s/center_of_mass_energy = 13000/center_of_mass_energy = ${ECM}/g" My-${PROCESS}.in
+# And now the PDF sets
+sed -i -e "s/CT14lo/${PDFSETLO}/g" My-${PROCESS}.in
+sed -i -e "s/CT14nlo/${PDFSETNLO}/g" My-${PROCESS}.in
 
 # Check whether all shared object libraries are resolved
 ldd ${EXEDIR}/resummino
